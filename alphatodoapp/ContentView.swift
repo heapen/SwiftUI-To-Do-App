f8e53699a77c807432ba7fdf5fda4e317ef96e8a
heapen <alparslanbatuhancelik@gmail.com>
2024-12-06 01:53:13 +0300

    Initial Commit

---
+++ alphatodoapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  alphatodoapp
+//
+//  Created by Batuhan Çelik on 6.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ alphatodoapp/alphatodoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  alphatodoappApp.swift
+//  alphatodoapp
+//
+//  Created by Batuhan Çelik on 6.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct alphatodoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
